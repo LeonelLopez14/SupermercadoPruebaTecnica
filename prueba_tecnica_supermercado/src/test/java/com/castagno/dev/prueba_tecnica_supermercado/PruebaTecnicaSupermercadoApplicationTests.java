c1f434825d1e413d6982d0a56e2029128733ca35
LeonelLopez14 <ll.lopezleonel99@gmail.com>
2026-03-27 14:15:38 -0300

    V0: creacion del proyecto y repo

---
+++ prueba_tecnica_supermercado/src/test/java/com/castagno/dev/prueba_tecnica_supermercado/PruebaTecnicaSupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castagno.dev.prueba_tecnica_supermercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSupermercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
